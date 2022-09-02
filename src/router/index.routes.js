ebd0e1911565b5ced8c72a2bd32a3bff1914b5fb
Jose David <davidherrera0429@gmail.com>
2022-09-02 14:37:32 -0500

    se rediseña la seccion de proyectos

--- src/router/index.routes.js
+++ src/router/index.routes.js
@@ -12,14 +12,14 @@ const  router = (route) => {
 
     body.appendChild(main);
     // main.appendChild(pages.header());
-    main.appendChild(pages.projects());
-    utils.showProjects();
+    // main.appendChild(pages.projects());
+    // utils.showProjects();
     // main.appendChild(pages.services());
-    // main.appendChild(pages.about());
+    main.appendChild(pages.about());
     // main.appendChild(pages.banner());
     // main.appendChild(pages.testimonials());
     // main.appendChild(pages.contact());
-    // main.appendChild(pages.footer());
+    main.appendChild(pages.footer());
 
 
     // switch(route) {
