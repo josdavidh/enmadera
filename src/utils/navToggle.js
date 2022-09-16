2788df7aa7c3eb2bd5bfa4bf623670eb9af3d513
Jose David <davidherrera0429@gmail.com>
2022-09-16 11:55:28 -0500

    se solucionan los errores del responsive

--- src/utils/navToggle.js
+++ src/utils/navToggle.js
@@ -2,6 +2,7 @@ export default () => {
     const body = document.querySelector("body");
     const navHamburgerBtn = document.querySelector(".nav__hamburguer");
     const navCloseBtn = document.querySelector(".nav__close");
+    const navLinks = document.querySelectorAll(".nav__links");
 
     const nav = document.querySelector(".nav")
 
@@ -14,5 +15,11 @@ export default () => {
     navCloseBtn.addEventListener("click", () => {
         nav.classList.toggle("show")
         body.style.overflowY = "auto";
+    })
+
+    navLinks.forEach( element => {
+        element.addEventListener("click", ()=>{
+            body.style.overflowY = "auto";
+        })
     })
 }
